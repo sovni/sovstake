813a82fec37473a618c09a62bb4fc075533a8913
sovni <christophe.burgalat@gmail.com>
2022-02-15 16:33:16 +0100

    Improve stake contract

--- contracts/SovStake.sol
+++ contracts/SovStake.sol
@@ -1,10 +1,10 @@
-// contracts/MyNFT.sol
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import '@openzeppelin/contracts/token/ERC20/IERC20.sol';
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 contract SovToken is ERC20 {
@@ -23,6 +23,8 @@ contract SovStake is Ownable{
     uint private tvl;
     uint private ratio;
 
+    using SafeMath for uint;
+
     constructor(address _stakeToken, address _priceFeed) {
         // DAI/ETH price Feed on Kovan testnet
         //priceFeed = AggregatorV3Interface(0x74825DbC8BF76CC4e9494d0ecB210f676Efa001D);
@@ -31,15 +33,15 @@ contract SovStake is Ownable{
         //dai = IERC20(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
         stakeToken = IERC20(_stakeToken);
         //rewardToken = IERC20(_stakeToken);
-        ratio = 0.01;
+        ratio = 100;
     }
 
     function stake(uint quantity) public {
         if (stakers[msg.sender] > 0 && stakersDate[msg.sender] > 0) {
             computeRewards(msg.sender);
         }
-        stakers[msg.sender] =  stakers[msg.sender] + quantity;
-        tvl += quantity;
+        stakers[msg.sender] =  stakers[msg.sender].add(quantity);
+        tvl = tvl.add(quantity);
         stakersDate[msg.sender] = block.timestamp;
         stakeToken.transferFrom(msg.sender, address(this), quantity);
     }
@@ -56,17 +58,17 @@ contract SovStake is Ownable{
     }
 
     function computeRewards(address staker) private {
-        require(stakers[msg.sender] == 0, "No staked token.");
-        require(stakersDate[msg.sender] == 0, "No stake date registered.");
+        require(stakers[staker] == 0, "No staked token.");
+        require(stakersDate[staker] == 0, "No stake date registered.");
 
-        int rewards = (getLatestPrice() * stakers[msg.sender]) * (block.timestamp - stakersDate[msg.sender]) * ratio;
+        uint rewards = (getLatestPrice().mul(stakers[staker])) * (block.timestamp - stakersDate[staker]) / ratio;
 
-        stakersDate[msg.sender] = block.timestamp;
-        rewardToken.transfer(msg.sender, rewards);
+        stakersDate[staker] = block.timestamp;
+        rewardToken.transfer(staker, rewards);
     }
    
     // Return latest price of DIA in ETH
-    function getLatestPrice() public view returns (int) {
+    function getLatestPrice() public view returns (uint) {
         (
             uint80 roundID, 
             int price,
@@ -74,6 +76,6 @@ contract SovStake is Ownable{
             uint timeStamp,
             uint80 answeredInRound
         ) = priceFeed.latestRoundData();
-        return price;
+        return uint(price);
     }
 }
