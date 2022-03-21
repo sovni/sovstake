38558bec27124d0f4684cf22183576abd7fdbc22
sovni <christophe.burgalat@gmail.com>
2022-03-21 23:57:56 +0100

    Update smart contract and contract deployment js

--- contracts/SovStake.sol
+++ contracts/SovStake.sol
@@ -21,13 +21,12 @@ contract SovToken is ERC20 {
 
 
 contract SovStake is Ownable{
-    //IERC20 stakeToken;
-    Dai stakeToken;
-    SovToken rewardToken;
+    IERC20 stakeToken;
+    IERC20 rewardToken;
 
     mapping(address => uint) private stakers;
     mapping(address => uint) private stakersDate;
-    AggregatorV3Interface internal priceFeed;
+    //AggregatorV3Interface internal priceFeed;
     uint private tvl;
     uint private ratio;
 
@@ -36,16 +35,17 @@ contract SovStake is Ownable{
 
     using SafeMath for uint;
 
-    constructor(address _stakeToken, address _priceFeed) {
+    constructor(address _rewardToken, address _stakeToken) {//}, address _priceFeed) {
         // DAI/ETH price Feed on Kovan testnet
         //priceFeed = AggregatorV3Interface(0x74825DbC8BF76CC4e9494d0ecB210f676Efa001D);
-        priceFeed = AggregatorV3Interface(_priceFeed);
+        //priceFeed = AggregatorV3Interface(_priceFeed);
         // injecter l'address du token Dai à utiliser (Kovan)
         //dai = IERC20(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
-        //stakeToken = IERC20(_stakeToken);
-        //rewardToken = IERC20(_stakeToken);
+        stakeToken = IERC20(_stakeToken);
+        rewardToken = IERC20(_rewardToken);
         ratio = 100;
     }
+
 
     function stake(uint quantity) public {
         if (stakers[msg.sender] > 0 && stakersDate[msg.sender] > 0) {
@@ -59,26 +59,26 @@ contract SovStake is Ownable{
     }
 
     function withdraw() public {
-        require(stakers[msg.sender] == 0, "No staked token.");
-        require(stakersDate[msg.sender] == 0, "No stake date registered.");
+        require(stakers[msg.sender] > 0, "No staked token.");
+        require(stakersDate[msg.sender] != 0, "No stake date registered.");
         
         computeRewards(msg.sender);
         uint quantity = stakers[msg.sender];
         stakers[msg.sender] = 0;
         stakersDate[msg.sender] = 0;
         tvl = tvl.sub(quantity);
-        stakeToken.transfer(msg.sender, quantity);        
+        stakeToken.transferFrom(address(this), msg.sender, quantity);        
         emit TokenWithdrawn();
     }
 
     function computeRewards(address staker) private {
-        require(stakers[staker] == 0, "No staked token.");
-        require(stakersDate[staker] == 0, "No stake date registered.");
+        require(stakers[staker] > 0, "No staked token.");
+        require(stakersDate[staker] != 0, "No stake date registered.");
 
-        uint rewards = (getLatestPrice().mul(stakers[staker])) * (block.timestamp - stakersDate[staker]) / ratio;
+        uint rewards = 10;//(getLatestPrice().mul(stakers[staker])) * (block.timestamp - stakersDate[staker]) / ratio;
 
         stakersDate[staker] = block.timestamp;
-        rewardToken.transfer(staker, rewards);
+        rewardToken.transferFrom(address(this), staker, rewards);
     }
 
     function getTVL() public view returns (uint) {
@@ -89,14 +89,8 @@ contract SovStake is Ownable{
         return stakers[msg.sender];
     }   
 
-    function faucet(address beneficiary) public onlyOwner {
-        require(stakeToken.balanceOf(msg.sender) > 100000000000000000000, "Not enought token");
-        stakeToken.transferFrom(msg.sender, beneficiary, 100000000000000000000);
-    }
-
-
     // Return latest price of DIA in ETH
-    function getLatestPrice() public view returns (uint) {
+    /*function getLatestPrice() public view returns (uint) {
         (
             uint80 roundID, 
             int price,
@@ -105,5 +99,5 @@ contract SovStake is Ownable{
             uint80 answeredInRound
         ) = priceFeed.latestRoundData();
         return uint(price);
-    }
+    }*/
 }
--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -3,7 +3,9 @@ var SovToken = artifacts.require("SovToken");
 var DaiToken = artifacts.require("Dai");
 
 module.exports = function(deployer) {
-  deployer.deploy(SovToken);
-  deployer.deploy(DaiToken);
-  deployer.deploy(SovStake, '0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa', '0x74825DbC8BF76CC4e9494d0ecB210f676Efa001D');
+  deployer.deploy(SovToken)
+  .then(() =>
+    deployer.deploy(DaiToken)
+    .then(() =>
+      deployer.deploy(SovStake, SovToken.address, DaiToken.address)));
 };
